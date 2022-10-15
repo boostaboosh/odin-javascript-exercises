126d89395bfacc6820564499ac26d9c8368a6cff
boostaboosh <mail.laurencesnow@gmail.com>
2022-10-15 13:09:39 +0100

    Complete repeatString exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfRepetions) { 
+    
+    if (numberOfRepetions < 0) { return 'ERROR' };
 
+    returnString = '';
+
+    for ( let repetition = 0; repetition < numberOfRepetions; repetition++ ) {
+        returnString += string;
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
