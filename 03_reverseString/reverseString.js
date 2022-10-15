76ba8ac664eb23c407701d02d4bf9328785f4c0e
boostaboosh <mail.laurencesnow@gmail.com>
2022-10-15 15:09:48 +0100

    Complete reverse string exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
+let reverseArray = function reverseOrderOfElementsInArray (array) {
+    let reversedArray = [];
+    
+    while ( array.length > 0 ) {
+        let lastLetter = array.pop();
+        reversedArray.push(lastLetter);
+    }
 
+    return reversedArray; 
+}
+
+const reverseString = function(stringToReverse) {
+    let arrayToReverse = stringToReverse.split('');
+
+    let reversedArray = reverseArray(arrayToReverse);
+    
+    let reversedString = reversedArray.join('');
+    
+    return reversedString;
 };
 
 // Do not edit below this line
