1f491f71a7bf674f9be781986025871431291c64
boostaboosh <mail.laurencesnow@gmail.com>
2022-10-16 18:03:32 +0100

    Commit exercise05 "sum all"

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function( numberOne , numberTwo ) {
+    if ( typeof(numberOne) !== 'number' || typeof(numberTwo) !== 'number' ) { return 'ERROR' }
+    else if ( numberOne < 0 || numberTwo < 0 ) {return 'ERROR'}
+    else if ( numberOne === numberTwo ) { return numberOne }
+    else {
+        let biggerNumber = numberOne > numberTwo ? numberOne : numberTwo;
+        let smallerNumber = biggerNumber === numberOne ? numberTwo : numberOne; 
 
+        let sumOfAll = 0;
+        for (let numberIteration = smallerNumber; numberIteration <= biggerNumber; numberIteration++) {
+            sumOfAll += numberIteration;
+        }
+
+        return sumOfAll;
+    }
 };
 
 // Do not edit below this line
