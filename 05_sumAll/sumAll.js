16100f4d453e42eded4c5a18e88f326d345b6721
boostaboosh <mail.laurencesnow@gmail.com>
2022-10-16 18:11:58 +0100

    Add check for parameters being integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,5 @@
 const sumAll = function( numberOne , numberTwo ) {
-    if ( typeof(numberOne) !== 'number' || typeof(numberTwo) !== 'number' ) { return 'ERROR' }
+    if ( ! Number.isInteger( numberOne ) || ! Number.isInteger( numberTwo ) ) { return 'ERROR' }
     else if ( numberOne < 0 || numberTwo < 0 ) {return 'ERROR'}
     else if ( numberOne === numberTwo ) { return numberOne }
     else {
