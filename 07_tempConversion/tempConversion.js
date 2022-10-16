cde623e8a0ff0834d9d2a91197d23f1ec1d5005e
boostaboosh <mail.laurencesnow@gmail.com>
2022-10-16 22:19:25 +0100

    Complete exercise 07 'temp conversion'

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
+let roundToOneDecimalPlace = function roundNumberToOneDecimalPlace ( number ) {
+  return Math.round( number * 10 ) / 10;
+}
 
+const ftoc = function( fahrenheitTemperature ) {
+  let temperatureInCelsiusScale = ( fahrenheitTemperature - 32 ) * ( 5 / 9 );
+  
+  return roundToOneDecimalPlace(temperatureInCelsiusScale);
 };
 
-const ctof = function() {
-
+const ctof = function( celsiusTemperature ) {
+  let temperatureInFahrenheitScale = ( celsiusTemperature * ( 9 / 5 ) ) + 32;
+  
+  return roundToOneDecimalPlace(temperatureInFahrenheitScale);
 };
 
 // Do not edit below this line
