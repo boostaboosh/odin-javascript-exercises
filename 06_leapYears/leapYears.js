69f43de4e20b76fa3beccb69ab3c7d2e1fc46abc
boostaboosh <mail.laurencesnow@gmail.com>
2022-10-16 18:33:48 +0100

    Complete exercise 06 'leap years'

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,32 @@
-const leapYears = function() {
+let isDivisibleByFour = function isNumberDivisibleByFour ( number ) {
+    if ( number % 4 === 0 ) { return true }
+    else { return false }
+}
 
+let isDivisibleByOneHundred = function isNumberDivisibleByOneHundred ( number ) {
+    if ( number % 100 === 0 ) { return true }
+    else { return false }
+}
+
+let isDivisibleByFourHundred = function isNumberDivisibleByFourHundred ( number ) {
+    if ( number % 400 === 0 ) { return true }
+    else { return false }
+}
+
+const leapYears = function( year ) {
+    let isLeapYear = false;
+
+    if ( isDivisibleByFour( year ) ) {
+        isLeapYear = true;
+        if ( isDivisibleByOneHundred( year ) ) {
+            isLeapYear = false;
+            if ( isDivisibleByFourHundred ( year ) ) {
+                isLeapYear = true;
+            }
+        }
+    }
+
+    return isLeapYear;
 };
 
 // Do not edit below this line
